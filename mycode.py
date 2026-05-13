2ebdec45412f4df6ec4e1d843d7aca8739d11c3c
abrahamjain7 <abrahamjain777@gmail.com>
2026-05-13 12:39:39 +0530

    made the second change

--- mycode.py
+++ mycode.py
@@ -1,8 +1,6 @@
 import mymodule
 print("my first line")
 print("my second line")
-<<<<<<< HEAD
-print("my latest third line")
-=======
 print("my most latest third line")
->>>>>>> week1-client-changes
+
+print("CHANGE FOR SECOND COMMIT")
